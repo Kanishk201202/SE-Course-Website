e8f3917bc3ea20f4df32f4200668cb281cfce203
Kanishk201202 <110833104+Kanishk201202@users.noreply.github.com>
2022-09-19 14:35:56 +0530

    Updated GCU

--- src/components13/Gcu.js
+++ src/components13/Gcu.js
@@ -12,7 +12,7 @@ export default function GCU() {
                 <NavLink className="nav-link" to="/registerissue">Register Issue</NavLink>
             </li>
             <li className="nav-item">
-                <NavLink className="nav-link" to="/administartiveinterface">AdministrativeInterface</NavLink>
+                <NavLink className="nav-link" to="/administartiveinterface">Administrative Interface</NavLink>
             </li>
             <li className="nav-item">
                 <NavLink className="nav-link" to="/councellorinterface">Councellor Interface</NavLink>
